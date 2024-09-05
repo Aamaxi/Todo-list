10b2d0890887b0b7487ffb51d5ccfbda984e6114
Aamaxi <112830001+Aamaxi@users.noreply.github.com>
2024-09-05 15:52:48 +0200

    la till ny stil på lista och gjorde att man kan lägga till uppgifter med specifik text och att ta bort uppgifter

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 function addTask() {
   let divTask = document.createElement("div");
+
+  let input = document.querySelector("#list-text");
+  let inputValue = input.value;
+
   divTask.classList.add('list-item');
   divTask.innerHTML = `
-    <p class=\"list-item-p\">Do the dishes</p>
-    <button>Finish this task</button>
+    <p class=\"list-item-p\">${inputValue}</p>
+    <button onclick="finishTask(this)">Finish this task</button>
   `;
 
   let container = document.querySelector('section');
   container.appendChild(divTask);
 };
+
+
+function finishTask(element) {
+  element.parentElement.remove()
+  emptyChecker()
+}
+
+function emptyChecker() {
+  container = document.querySelector("section")
+  if (!container.hasChildNodes() == True) {
+    // Lägg till text som säger att det finns inga tasks
+  }
+}
